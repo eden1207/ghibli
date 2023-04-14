f1a884f1ed18ff7d447ff981a1a89636003a57ff
eden1207 <alexis.mauray@gmail.com>
2023-04-14 19:01:16 +0200

    Cinquième mise à jour du dossier src/

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,6 +6,10 @@ import castle_helix2 from '../../assets/castle_helix2.png'
 import castle_flag from '../../assets/castle_flag.png'
 import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 import { HiOutlineSearch } from "react-icons/hi";
+import sky_cloud_background from '../../assets/sky_cloud_background.png'
+import moving_sky_cloud1 from '../../assets/moving_sky_cloud1.png'
+import moving_sky_cloud2 from '../../assets/moving_sky_cloud2.png'
+import castle_clothes from '../../assets/castle_clothes.png'
 
 
 function SearchBar() {
@@ -97,6 +101,16 @@ export default function Banner() {
                 <img src={castle_helix1} className='castlehelix1 castle-helix-rotation1' alt='castle helix 1' />
                 <img src={castle_helix2} className='castlehelix2 castle-helix-rotation2' alt='castle helix 2' />
                 <img src={castle_flag} className='castleflag castle-flag-scaling' alt='castle flag' />
+                <img src={castle_clothes} className='castleclothes castle-clothes-scaling' alt='castle clothes' />
+            </div>
+            <div className='moving-clouds-content'>
+                <img src={moving_sky_cloud2} className='moving-cloud1 cloud-translation4' alt='moving cloud 1' />
+                <img src={moving_sky_cloud1} className='moving-cloud2 cloud-translation3' alt='moving cloud 2' />
+                <img src={moving_sky_cloud2} className='moving-cloud3 cloud-translation2' alt='moving cloud 3' />
+                <img src={moving_sky_cloud1} className='moving-cloud4 cloud-translation1' alt='moving cloud 4' />
+            </div>
+            <div className='sky-cloud-background-content'>
+                <img src={sky_cloud_background} className='sky-cloud-background' alt='sky cloud' />
             </div>
             <SearchBar />
             <ToolListing />
@@ -105,6 +119,14 @@ export default function Banner() {
 }
 
 /*
+cloud-translation4
+cloud-translation3
+cloud-translation2
+cloud-translation1
+ */
+
+/*
+castle-translation
 <div className='castle-content castle-translation'>
 <img src={castle_body} className='castlebody' alt='castle body' />
 <img src={castle_helix1} className='castlehelix1 castle-helix-rotation1' alt='castle helix 1' />
